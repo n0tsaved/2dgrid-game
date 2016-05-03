67fb47c7e66c6699366dd5eafa5fd3428dee87aa
n0tsaved <f.lentisco@studenti.unina.it>
2016-05-03 18:10:24 +0200

    generatore di enviroment indoor Definitivo

--- tilemap/IndoorMapGenerator.java
+++ tilemap/IndoorMapGenerator.java
@@ -3,7 +3,6 @@ package tilemap;
 import java.util.Random;
 
 /**
- * Created by merda on 29/04/16.
  */
 public class IndoorMapGenerator extends MapGenerator {
     private final int ROOM_MAX_SIZE=15;
@@ -24,22 +23,22 @@ public class IndoorMapGenerator extends MapGenerator {
             // split vertically
             int new_width=r.nextInt(((width/4)*3)-((width/4)+1))+(width/4);
             int x_start_wall=new_width+x_offset; //width/2 +x_offset;
-
-            splitArea(m,new_width, height,x_offset, y_offset, !vert); //split sx area
-            splitArea(m,width-new_width, height,x_start_wall+1, y_offset, !vert); //split dx area
             for(int j=y_offset; j<GameMap.HEIGHT && !m[x_start_wall][j].isBlocked; j++)
                 m[x_start_wall][j].isBlocked=true;
             m[x_start_wall][r.nextInt(height/2)+y_offset].isBlocked=false;
+            splitArea(m,new_width, height,x_offset, y_offset, !vert); //split sx area
+            splitArea(m,width-new_width, height,x_start_wall+1, y_offset, !vert); //split dx area
+
         }else{
             //split horizontally
             int new_height=r.nextInt(((3/4)*height)-((1/4)*height)+1)+(height/4);
             int y_start_wall=new_height + y_offset;
-
-            splitArea(m, width,new_height, x_offset, y_offset, !vert); //split up area
-            splitArea(m, width, height-new_height, x_offset, y_start_wall+1, !vert); //split down area;
             for(int i=x_offset; i<GameMap.WIDTH && !m[i][y_start_wall].isBlocked; i++)
                 m[i][y_start_wall].isBlocked=true;
             m[r.nextInt(width/2)+x_offset][y_start_wall].isBlocked=false;
+            splitArea(m, width,new_height, x_offset, y_offset, !vert); //split up area
+            splitArea(m, width, height-new_height, x_offset, y_start_wall+1, !vert); //split down area;
+
         }
 
     }
